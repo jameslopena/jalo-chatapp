8abf8c1587fb52f1b04ce0dea1f0c531d0b9fb0d
James Lopena <lopena.james@gmail.com>
2016-03-08 20:23:15 +0100

    add bower dependencies and index html

--- server.js
+++ server.js
@@ -3,8 +3,18 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res){
-   res.end("test");
-}).listen(process.env.PORT || 8080, function(){
+    res.sendFile(__dirname + "/server/views/index.html");
+    //res.render("index")
+})
+
+var port = process.env.PORT || 8080
+app.listen(port, function(){
     console.log("listening..");
 });
+
